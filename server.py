b71c5b2e7934f12f8f984731f7c6e9e435dc609d
JT Raber <JMANN240raber@gmail.com>
2022-09-12 15:23:14 -0400

    colby restful smartass

--- server.py
+++ server.py
@@ -1,13 +1,20 @@
 from fastapi import FastAPI, UploadFile
 from fastapi.responses import FileResponse
-
+from pathlib import Path
 app = FastAPI()
 
-@app.get("/file", response_class=FileResponse)
+@app.get("/file")
 def get_file(remote_file_path: str):
-	return FileResponse(remote_file_path)
+	path = Path(remote_file_path)
+	if path.is_file():
+		return FileResponse(remote_file_path)
+	else:
+		return {
+			"status": "failure",
+			"reason": f"'{remote_file_path}' is not a file on the server."
+		}
 
-@app.post("/file")
+@app.put("/file")
 async def post_file(remote_file: UploadFile):
 	with open(remote_file.filename, 'wb') as local_file:
 		local_file.write(await remote_file.read())
